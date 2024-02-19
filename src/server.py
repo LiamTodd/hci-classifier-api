4cb6898c8a6ec481940ded4dc34a6b86c36d6a9c
LiamTodd <toddliam26@gmail.com>
2024-02-19 15:02:04 +1100

    Implement bulk classifier endpoint.

--- src/server.py
+++ src/server.py
@@ -1,30 +1,30 @@
-import dill
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
-from utils import map_raw_output_to_labels
+from src.utils import predict
+
 
 app = Flask(__name__)
+
+
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
-
-vectorizer = dill.load(open("resources/vectorizer", "rb"))
-classifier = dill.load(open("resources/classifier.pkl", "rb"))
 
 
 @app.route("/classify", methods=["POST"])
 @cross_origin()
 def classify():
-
-    # Get the JSON data from the request
     request_data = request.get_json()
+    result = predict(request_data)
+    return jsonify(result)
 
-    # Dummy data to return
-    predictions = classifier.predict(vectorizer.transform(request_data.values())).toarray().tolist()
-    result = {}
-    for i, label in enumerate(request_data.keys()):
-        result[label] = {"content": request_data[label], "predictions": map_raw_output_to_labels(predictions[i])}
 
-    # Return the dummy data as JSON
+@app.route("/bulk-classify", methods=["POST"])
+@cross_origin()
+def bulk_classify():
+    request_data = request.get_json()
+    result = []
+    for issue in request_data:
+        result.append(predict(issue))
     return jsonify(result)
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,32 @@
+import dill
+
 APP_USAGE = "app usage"
 INCLUSIVENESS = "inclusiveness"
 USER_REACTION = "user reaction"
-NON_HC = "non hc"
+NON_HC = "non human-centric"
+
 
 def map_raw_output_to_labels(raw_output):
     result = {}
-    result[APP_USAGE] = True if raw_output[0] == 1 else False
-    result[INCLUSIVENESS] = True if raw_output[1] == 1 else False
-    result[USER_REACTION] = True if raw_output[2]== 1 else False
-    result[NON_HC] = True if raw_output[3] == 1 else False
+    result[APP_USAGE] = bool(raw_output[0])
+    result[INCLUSIVENESS] = bool(raw_output[1])
+    result[USER_REACTION] = bool(raw_output[2])
+    result[NON_HC] = bool(raw_output[3])
     return result
 
-    
+
+vectorizer = dill.load(open("resources/vectorizer", "rb"))
+classifier = dill.load(open("resources/classifier.pkl", "rb"))
+
+
+def predict(data):
+    predictions = (
+        classifier.predict(vectorizer.transform(data.values())).toarray().tolist()
+    )
+    result = {}
+    for i, label in enumerate(data.keys()):
+        result[label] = {
+            "content": data[label],
+            "predictions": map_raw_output_to_labels(predictions[i]),
+        }
+    return result
